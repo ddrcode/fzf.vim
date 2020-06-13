136bd7f7f0953dd20137729a1c9892fe20e0a65e
dejf <dejf@Mac-Pro.local>
2020-06-13 20:11:50 +0100

    viu support

--- bin/showimg.sh
+++ bin/showimg.sh
@@ -2,10 +2,16 @@
 
 FILE=$1
 
+has() {
+  command -v $1 > /dev/null
+}
+
 show_with_kitty() {
-  if (kitty +kitten icat --detect-support --detection-timeout 0.1 2> /dev/null); then echo "koza";
+  if (has kitty && has convert && kitty +kitten icat --detect-support --detection-timeout 0.1 2> /dev/null); then 
+    kitty +kitten icat --align left --transfer-mode stream  $1
+  elif (has viu); then
+    viu $1
   fi
-  kitty +kitten icat --align left --transfer-mode stream  $1
 }
 
 show_with_kitty $1
