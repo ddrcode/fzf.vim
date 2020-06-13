826be86afe8974772a14d8432af36cb64e979d2f
ddrcode <ddrcode@gmail.com>
2020-06-13 22:27:50 +0100

    w3img support

--- bin/showimg.sh
+++ bin/showimg.sh
@@ -2,13 +2,17 @@
 
 FILE=$1
 
-has() {
-  command -v $1 > /dev/null
-}
 
 show_with_kitty() {
+  has() { command -v $1 > /dev/null; }
+  w3mimg="/usr/lib/w3m/w3mimgdisplay"
+  
   if (has kitty && has convert && kitty +kitten icat --detect-support --detection-timeout 0.1 2> /dev/null); then 
     kitty +kitten icat --align left --transfer-mode stream  $1
+  # elif (has fim); then
+  #   fim -a $1
+  elif [[ $TERM == *"rxvt" && -f "$w3mimg" ]]; then
+    echo -e '0;1;0;0;200;160;;;;;ant.jpg\n4;\n3;' | eval $w3mimg
   elif (has viu); then
     viu $1
   fi
